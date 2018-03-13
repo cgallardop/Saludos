c53208616f2eb24da9de2bfac413e598fefe7e41
cgallardop <cgallardo@etsisi.upm.es>
2018-03-13 13:17:19 +0100

    Initial commit

---
+++ hola.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : hola.cpp
+// Author      : CGP
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void ramaSecreta();
+void ramaSecretaVersion1_0();
+
+
+int main() {
+	
+	cout << "!!!Hello World!!!" << endl; // Cambio en comentario 1
+	cout << " Hola Mundo!! " << endl;			// comentario 2
+	cout << "Bon jour!!!" << endl;					// comentarios 3
+	return 0;
+}
+
+
+
+void ramaSecretaVersion1_0() {
+	cout << "Rama secreta v1.0 \n" ;	
+	cout << "Change in rama secreta v1.0 \n" ;	
+}
+
+
